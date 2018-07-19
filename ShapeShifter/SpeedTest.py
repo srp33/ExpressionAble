120c0f54dbd680c1ec21ccd0949ad5286b066bdc
frytime32 <31867221+frytime32@users.noreply.github.com>
2018-07-19 10:44:06 -0600

    Speed test ready for testing all files and operations

--- ShapeShifter/SpeedTest.py
+++ ShapeShifter/SpeedTest.py
@@ -13,10 +13,14 @@ smallFilters=[None,"discrete1=='hot'","float1>2","discrete1=='hot' and float1>2"
 smallColumns=[[],['Sample','discrete1'],['Sample','float1'],['Sample','discrete1','float1'],['Sample','discrete1','discrete2'],['Sample','float1','float2'],['Sample','discrete1','discrete2','float1','float2']]
 mediumFilters=[None,"discrete10=='hot'", "int10>20", "discrete10=='hot' and int10>20", "discrete10=='hot' and discrete20=='cold'", "int10>20 and int20<70", "discrete10=='hot' and discrete20=='cold' and int10>20 and int20<70"]
 mediumColumns=[[],['Sample','discrete10'],['Sample','int10'],['Sample','discrete10','int10'],['Sample','discrete10','discrete20'],['Sample','int10','int20'],['Sample','discrete10','discrete20','int10','int20']]
-
+metabricColumns = [[],['Sample','OS_STATUS'], ['Sample','10-Mar'], ['Sample','OS_STATUS','10-Mar'], ['Sample','OS_STATUS', 'HORMONE_THERAPY'], ['Sample','10-Mar', '2-Mar'], ['Sample','OS_STATUS', 'HORMONE_THERAPY','10-Mar', '2-Mar'] ]
+metabricFilters=[None,"OS_STATUS=='LIVING'", '10-Mar>5.8', "OS_STATUS=='LIVING' and 10-Mar>5.8", "OS_STATUS=='LIVING' and HORMONE_THERAPY=='YES'", '10-Mar>5.8 and 2-Mar>5.8', "OS_STATUS=='LIVING' and HORMONE_THERAPY=='YES' and 10-Mar>5.8 and 2-Mar>5.8"]
+tallColumns = [[],['Sample','int2'], ['Sample','discrete2'], ['Sample','discrete2','int2'], ['Sample','discrete2','discrete4'], ['Sample','int2','int4'], ['Sample','discrete2','discrete4', 'int2','int4']]
+tallFilters = [None,"discrete2=='hot'", "int2>20", "discrete2=='hot' and int2>20", "discrete2=='hot' and discrete4=='cold'", "int2>20 and int4<70", "discrete2=='hot' and discrete4=='cold' and int2>20 and int4<70"]
 header='Operation\tFormat\tSize\tFilter\tSeconds\n'
 
 outputLines=[]
+filterLines=[]
 #Read small file
 for i in range(0,len(filetypes)):
     for j in range(0, len(filterDescriptions)):
@@ -28,6 +32,15 @@ for i in range(0,len(filetypes)):
         seconds = str(t2-t1)
         line = "Read\t"+filetypes[i]+"\tSmall\t"+filterDescriptions[j]+"\t"+seconds+"\n"
         outputLines.append(line)
+        if smallFilters[j]==None:
+            seconds='0'
+        else:
+            t1 = time.time()
+            df = df.query(smallFilters[j])
+            t2 = time.time()
+            seconds = str(t2 - t1)
+        line = "Filter\t" + filetypes[i] + "\tSmall\t" + filterDescriptions[j] + "\t" + seconds + "\n"
+        filterLines.append(line)
 
 #Write small file
 for i in range(0,len(filetypes)):
@@ -54,8 +67,17 @@ for i in range(0,len(filetypes)):
         seconds = str(t2-t1)
         line = "Read\t"+filetypes[i]+"\tMedium\t"+filterDescriptions[j]+"\t"+seconds+"\n"
         outputLines.append(line)
+        if mediumFilters[j] == None:
+            seconds = '0'
+        else:
+            t1 = time.time()
+            df = df.query(metabricFilters[j])
+            t2 = time.time()
+            seconds = str(t2 - t1)
+        line = "Filter\t" + filetypes[i] + "\tMedium\t" + filterDescriptions[j] + "\t" + seconds + "\n"
+        filterLines.append(line)
 
-#Write small file
+#Write medium file
 for i in range(0,len(filetypes)):
     for j in range(0, len(filterDescriptions)):
         fileName = "Tests/Speed/Medium/MediumTest.pq"
@@ -69,12 +91,84 @@ for i in range(0,len(filetypes)):
         line = "Write\t"+filetypes[i]+"\tMedium\t"+filterDescriptions[j]+"\t"+seconds+"\n"
         outputLines.append(line)
 
+#Read METABRIC file
+for i in range(0,len(filetypes)):
+    for j in range(0, len(filterDescriptions)):
+        fileName = "Tests/Speed/METABRIC/metabric." +extensions[i]
+        metabricFile = SSFile.factory(fileName)
+        t1=time.time()
+        df=metabricFile.read_input_to_pandas(columnList=metabricColumns[j])
+        t2=time.time()
+        seconds = str(t2-t1)
+        line = "Read\t"+filetypes[i]+"\tMETABRIC\t"+filterDescriptions[j]+"\t"+seconds+"\n"
+        outputLines.append(line)
+        if metabricFilters[j] == None:
+            seconds = '0'
+        else:
+            t1 = time.time()
+            df = df.query(metabricFilters[j])
+            t2 = time.time()
+            seconds = str(t2 - t1)
+        line = "Filter\t" + filetypes[i] + "\tMETABRIC\t" + filterDescriptions[j] + "\t" + seconds + "\n"
+        filterLines.append(line)
+
+#Write METABRIC file
+for i in range(0,len(filetypes)):
+    for j in range(0, len(filterDescriptions)):
+        fileName = "Tests/Speed/METABRIC/metabric.pq"
+        inFile = SSFile.factory(fileName)
+        outFile = SSFile.factory("Tests/Speed/Output/METABRICOutput"+str(j)+"." + extensions[i])
+        df = inFile._filter_data(columnList=metabricColumns[j], query=metabricFilters[j])
+        t1 = time.time()
+        outFile.write_to_file(df)
+        t2=time.time()
+        seconds = str(t2-t1)
+        line = "Write\t"+filetypes[i]+"\tMETABRIC\t"+filterDescriptions[j]+"\t"+seconds+"\n"
+        outputLines.append(line)
+
+#Read Tall file
+for i in range(0,len(filetypes)):
+    for j in range(0, len(filterDescriptions)):
+        fileName = "Tests/Speed/Tall/TallData." +extensions[i]
+        tallFile = SSFile.factory(fileName)
+        t1=time.time()
+        df= tallFile.read_input_to_pandas(columnList=tallColumns[j])
+        t2=time.time()
+        seconds = str(t2-t1)
+        line = "Read\t"+filetypes[i]+"\tTall\t"+filterDescriptions[j]+"\t"+seconds+"\n"
+        outputLines.append(line)
+        if tallFilters[j] == None:
+            seconds = '0'
+        else:
+            t1 = time.time()
+            df = df.query(tallFilters[j])
+            t2 = time.time()
+            seconds = str(t2 - t1)
+        line = "Filter\t" + filetypes[i] + "\tTall\t" + filterDescriptions[j] + "\t" + seconds + "\n"
+        filterLines.append(line)
+
+#Write Tall file
+for i in range(0,len(filetypes)):
+    for j in range(0, len(filterDescriptions)):
+        fileName = "Tests/Speed/Tall/TallData.pq"
+        inFile = SSFile.factory(fileName)
+        outFile = SSFile.factory("Tests/Speed/Output/TallOutput"+str(j)+"." + extensions[i])
+        df = inFile._filter_data(columnList=tallColumns[j], query=tallFilters[j])
+        t1 = time.time()
+        outFile.write_to_file(df)
+        t2=time.time()
+        seconds = str(t2-t1)
+        line = "Write\t"+filetypes[i]+"\tTall\t"+filterDescriptions[j]+"\t"+seconds+"\n"
+        outputLines.append(line)
+
 
 
 #Write all data to results file
 with open('Results.tsv','w') as outFile:
     outFile.write(header)
     for line in outputLines:
+        outFile.write(line)
+    for line in filterLines:
         outFile.write(line)
 
 

fa7d5c8a6f74050f1b55bf9a12a8324c5a87eb62
frytime32 <frytime32@gmail.com>
2018-07-20 08:53:57 -0600

    changed METABRIC filters

--- ShapeShifter/SpeedTest.py
+++ ShapeShifter/SpeedTest.py
@@ -5,16 +5,16 @@ from SSFile import SSFile
 #add tests for reading the whole file and a tall dataset (10 cols, 1 million rows)
 #later, support open science framework for storing large files
 #seperate filter logic for timing
-#currently excluding SQLite (db) and HTML
-filetypes = ['ARFF','CSV','Excel','GCT','HDF5','JSON','MsgPack','Parquet','Pickle','Stata','TSV']
-extensions=['arff','csv','xlsx','gct','hdf','json','mp','pq','pkl','dta','tsv']
+#currently excluding ARFF, SQLite (db) and HTML
+filetypes = ['CSV','Excel','GCT','HDF5','JSON','MsgPack','Parquet','Pickle','Stata','TSV']
+extensions=['csv','xlsx','gct','hdf','json','mp','pq','pkl','dta','tsv']
 filterDescriptions=['read_whole_file','1_categorical','1_numeric','1_of_each','2_categorical','2_numeric','2_of_each']
 smallFilters=[None,"discrete1=='hot'","float1>2","discrete1=='hot' and float1>2", "discrete1=='hot' and discrete2=='blue'", "float1>2 and float2<10", "discrete1=='hot' and discrete2=='blue' and float1>2 and float2<10"]
 smallColumns=[[],['Sample','discrete1'],['Sample','float1'],['Sample','discrete1','float1'],['Sample','discrete1','discrete2'],['Sample','float1','float2'],['Sample','discrete1','discrete2','float1','float2']]
 mediumFilters=[None,"discrete10=='hot'", "int10>20", "discrete10=='hot' and int10>20", "discrete10=='hot' and discrete20=='cold'", "int10>20 and int20<70", "discrete10=='hot' and discrete20=='cold' and int10>20 and int20<70"]
 mediumColumns=[[],['Sample','discrete10'],['Sample','int10'],['Sample','discrete10','int10'],['Sample','discrete10','discrete20'],['Sample','int10','int20'],['Sample','discrete10','discrete20','int10','int20']]
-metabricColumns = [[],['Sample','OS_STATUS'], ['Sample','10-Mar'], ['Sample','OS_STATUS','10-Mar'], ['Sample','OS_STATUS', 'HORMONE_THERAPY'], ['Sample','10-Mar', '2-Mar'], ['Sample','OS_STATUS', 'HORMONE_THERAPY','10-Mar', '2-Mar'] ]
-metabricFilters=[None,"OS_STATUS=='LIVING'", '10-Mar>5.8', "OS_STATUS=='LIVING' and 10-Mar>5.8", "OS_STATUS=='LIVING' and HORMONE_THERAPY=='YES'", '10-Mar>5.8 and 2-Mar>5.8', "OS_STATUS=='LIVING' and HORMONE_THERAPY=='YES' and 10-Mar>5.8 and 2-Mar>5.8"]
+metabricColumns = [[],['Sample','OS_STATUS'], ['Sample','A1BG'], ['Sample','OS_STATUS','A1BG'], ['Sample','OS_STATUS', 'HORMONE_THERAPY'], ['Sample','A1BG', 'A1CF'], ['Sample','OS_STATUS', 'HORMONE_THERAPY','A1BG', 'A1CF'] ]
+metabricFilters=[None,"OS_STATUS=='LIVING'", 'A1BG>5.8', "OS_STATUS=='LIVING' and A1BG>5.8", "OS_STATUS=='LIVING' and HORMONE_THERAPY=='YES'", 'A1BG>5.8 and A1CF>5.8', "OS_STATUS=='LIVING' and HORMONE_THERAPY=='YES' and A1BG>5.8 and A1CF>5.8"]
 tallColumns = [[],['Sample','int2'], ['Sample','discrete2'], ['Sample','discrete2','int2'], ['Sample','discrete2','discrete4'], ['Sample','int2','int4'], ['Sample','discrete2','discrete4', 'int2','int4']]
 tallFilters = [None,"discrete2=='hot'", "int2>20", "discrete2=='hot' and int2>20", "discrete2=='hot' and discrete4=='cold'", "int2>20 and int4<70", "discrete2=='hot' and discrete4=='cold' and int2>20 and int4<70"]
 header='Operation\tFormat\tSize\tFilter\tSeconds\n'

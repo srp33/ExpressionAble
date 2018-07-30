4ec992bf6ded5dfbc90bba7f8dba7f0b09166230
frytime32 <31867221+frytime32@users.noreply.github.com>
2018-07-30 09:50:49 -0600

    Revision to GCT

--- ShapeShifter/ConvertGCT.py
+++ ShapeShifter/ConvertGCT.py
@@ -4,13 +4,13 @@ import pandas as pd
 #The file will be named after the second parameter
 def toGCT(df, fileName):
     df = df.fillna("")
-    # Write #1.2 (the version string)
+#Write #1.2 (the version string)
     writeFile = open(fileName, 'w')
     writeFile.write("#1.2\n")
-    # Write the number of rows and the number of columns
+#Write the number of rows and the number of columns
     writeFile.write(str(len(df.index)))
     writeFile.write("\t" + str(len(df.columns[2:])) + "\n")
-    # Write the data
+#Write the data
     writeFile.write(str(df))
     writeFile.close()
 

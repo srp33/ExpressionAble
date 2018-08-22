4eab517a0d054e9ee5f5915488cd07ff7398432d
frytime32 <31867221+frytime32@users.noreply.github.com>
2018-08-22 08:21:13 -0600

    Refactoring Kallisto into two separate classes

--- ShapeShifter/KallistoTPMFile.py
+++ ShapeShifter/KallistoTPMFile.py
@@ -2,10 +2,10 @@ from SSFile import SSFile
 from kallisto import kallistoToPandas
 
 
-class KallistoFile(SSFile):
+class KallistoTPMFile(SSFile):
 
     def read_input_to_pandas(self, columnList=[], indexCol="Sample"):
-        df = kallistoToPandas(self.filePath)
+        df = kallistoToPandas(self.filePath, colName="tpm")
         #can I read in only certain columns?
         if len(columnList) > 0:
             df = df[columnList]

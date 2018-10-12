84ce99f56e5c8ccaf3b1ecc43918d0d21c929c8e
Kimball Hill <hillkimball@gmail.com>
2018-10-12 13:30:03 -0600

    removed bad import

--- shapeshifter/utils/__init__.py
+++ shapeshifter/utils/__init__.py
@@ -1,7 +1,6 @@
-__all__ = ['standardize_null_value', 'is_gzipped', 'build_parquet', 'salmon_to_pandas', 'kallisto_to_pandas',
+__all__ = ['is_gzipped', 'build_parquet', 'salmon_to_pandas', 'kallisto_to_pandas',
            'to_arff', 'to_gct', 'gct_to_pandas', 'is_date', 'is_numeric', 'arff_to_pandas']
 
-from Tests.CompareDataframes import standardize_null_value
 from shapeshifter.utils.convertarff import is_date, is_numeric, arff_to_pandas, to_arff
 from shapeshifter.utils.convertgct import to_gct, gct_to_pandas
 from shapeshifter.utils.kallisto import kallisto_to_pandas

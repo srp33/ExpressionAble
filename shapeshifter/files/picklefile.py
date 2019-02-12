07886816123e7880a32d20f1f5600748e3cd4bbc
Brandon Fry <frytime32@gmail.com>
2019-02-12 11:35:50 -0700

    removed unnecessary export_filter_results functions from subclasses

--- shapeshifter/files/picklefile.py
+++ shapeshifter/files/picklefile.py
@@ -12,15 +12,6 @@ class PickleFile(SSFile):
             df = df[columnList]
         return df
 
-    def export_filter_results(self, inputSSFile, column_list=[], query=None, transpose=False, include_all_columns=False,
-                              gzip_results=False, index_col="Sample"):
-        df = None
-        includeIndex = False
-        null = 'NA'
-        query, inputSSFile, df, includeIndex = super()._prep_for_export(inputSSFile, column_list, query, transpose,
-                                                                        include_all_columns, df, includeIndex, index_col)
-
-        self.write_to_file(df, gzip_results)
 
     def write_to_file(self, df, gzipResults=False, includeIndex=False, null='NA', indexCol="Sample", transpose=False):
         if not transpose:
--- shapeshifter/files/sqlitefile.py
+++ shapeshifter/files/sqlitefile.py
@@ -30,59 +30,6 @@ class SQLiteFile(SSFile):
         #     os.remove(filePath)
         return df
 
-    def export_filter_results(self, inputSSFile, column_list=[], query=None, transpose=False, include_all_columns=False,
-                              gzip_results=False, index_col="Sample"):
-
-        filePath=self.filePath #needs to be stored separately as a string, can't be turned to a file object
-        if query != None:
-            query = super()._translate_null_query(query)
-        if inputSSFile.isGzipped:
-            inputSSFile.filePath=gzip.open(inputSSFile.filePath)
-        df = inputSSFile._filter_data(columnList=column_list, query=query,
-                                      includeAllColumns=include_all_columns, indexCol=index_col)
-        null = 'NA'
-        includeIndex = False
-        if len(df.columns) > 999:
-            raise SizeExceededError("SQLite supports a maximum of 999 columns. Your data has " + str(
-                len(df.columns)) + " columns. Please use a smaller data set or consider using a different file type")
-            # print("Warning: SQLite supports a maximum of 999 columns. Your data has " + str(
-            #     len(df.columns)) + " columns. Extra data has been truncated.")
-            # df=df.iloc[:,0:999]
-        chunksize = 999//len(df.columns)
-        from sqlalchemy import create_engine
-
-        if gzip_results:
-            tempFile = tempfile.NamedTemporaryFile(delete=False)
-            engine = create_engine('sqlite:///' + tempFile.name)
-            table = filePath.split('.')[0]
-            tableList = table.split('/')
-            table = tableList[len(tableList) - 1]
-            if not transpose:
-                df = df.set_index(index_col) if index_col in df.columns else df.set_index(df.columns[0])
-
-                df.to_sql(table, engine, index=True, if_exists="replace", chunksize=chunksize)
-            else:
-                index_col = df.columns[0]
-                df = df.set_index(index_col) if index_col in df.columns else df.set_index(df.columns[0])
-
-                df = df.transpose()
-                df.to_sql(table, engine, if_exists="replace", index=True, index_label=index_col, chunksize=chunksize)
-            tempFile.close()
-            super()._gzip_results(tempFile.name, filePath)
-
-        else:
-            engine = create_engine('sqlite:///' + super()._remove_gz(filePath))
-            table = filePath.split('.')[0]
-            tableList = table.split('/')
-            table = tableList[len(tableList) - 1]
-            if not transpose:
-                df = df.set_index(index_col) if index_col in df.columns else df.set_index(df.columns[0])
-                df.to_sql(table, engine, if_exists='replace', chunksize=chunksize)
-            else:
-                index_col = df.columns[0]
-                df = df.set_index(index_col) if index_col in df.columns else df.set_index(df.columns[0])
-                df = df.transpose()
-                df.to_sql(table, engine, if_exists="replace", index=True, index_label=index_col, chunksize=chunksize)
 
     def write_to_file(self, df, gzipResults=False, includeIndex=False, null='NA', indexCol="Sample", transpose=False):
         filePath = self.filePath

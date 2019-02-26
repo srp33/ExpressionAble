db1c2d294924f81dbb44ce2b4166e4f4fccbe4ef
natemella <nathanmell@gmail.com>
2019-02-26 09:53:08 -0700

    I updated Tabulizer so that it includes the format that allows users to specify which table they want when a pdf has multiple tables. For example: "file.pdf?p=1&t=2"

--- shapeshifter/files/pdffile.py
+++ shapeshifter/files/pdffile.py
@@ -6,8 +6,7 @@ import os
 import subprocess
 import sys
 from tabula import errors
-import pandas as pd
-import numpy as np
+import re
 
 
 
@@ -16,34 +15,62 @@ class PDFFile(SSFile):
 
     def read_input_to_pandas(self, columnList = [], indexCol="Sample" ):
         os.getcwd()
-        user_input = self.filePath.split(':')
+        user_input = self.filePath.split('?')
+        print(user_input)
         if len(user_input) > 2:
             path = user_input[0:-1]
         else:
             path = user_input[0:2]
-        path = ':'.join(path)
+        path = '?'.join(path)
 
         def build_df(file, input_list):
             pgs = 'all'
             valid_pg_input = True
-            pg_input = input_list[-1].replace(",", "").replace("-", "")
+            pg_input = input_list[-1].replace(",", "").replace("-", "").replace("p=","").replace("&", "").replace("t=",
+            "")
+            t = False
+            p = False
+            tables = 0
             for char in pg_input:
                 if not char.isdigit():
                     valid_pg_input = False
-            if valid_pg_input == True or input_list[-1] == 'all':
-                pgs = input_list[-1]
-                print(pgs)
+            if valid_pg_input == True or 'all' in input_list:
+                input_list = input_list[-1].split("&")
+                for item in input_list:
+                    print(item)
+                    if item.startswith("p="):
+                        pgs = item.replace("p=","")
+                        p = True
+                    elif item.startswith("t="):
+                        tables = item.replace("t=","")
+                        tables = int(tables)
+                        tables -= 1
+                        t = True
+                if not p:
+                    print("No valid page input. Defaut page is \'all\'")
             else:
-                print("No valid page input. Default is page \'all\'")
+                print("No valid page input nor table input. Default is page \'all\' and default table is 1")
             try:
                 try:
                     try:
-                        data_frame = tabula.read_pdf(file, pages=pgs)
-
+                        if not t:
+                            data_frame = tabula.read_pdf(file, pages=pgs)
+                        else:
+                            print("multiple tables mode")
+                            my_list = tabula.read_pdf(file, pages=pgs, multiple_tables=True)
+                            data_frame = my_list[tables]
+                            transposed_df = my_list[tables].transpose()
+                            data_frame.columns = transposed_df[0]
+                            data_frame = data_frame.drop([0])
                     except tabula.errors.CSVParseError:
                         print("multiple tables mode")
+                        if not t:
+                            print("No valid table input. Default table is 1")
                         my_list = tabula.read_pdf(file, pages=pgs, multiple_tables=True)
-                        data_frame = pd.DataFrame(my_list)
+                        data_frame = my_list[tables]
+                        transposed_df = my_list[tables].transpose()
+                        data_frame.columns = transposed_df[0]
+                        data_frame = data_frame.drop([0])
                 except OSError:
                     print("Error: need to download Java 7 or higher")
             except subprocess.CalledProcessError:

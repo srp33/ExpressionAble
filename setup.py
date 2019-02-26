c6f0500a051ed10ef8d92ae2713c6ecf7fa3717f
Brandon Fry <frytime32@gmail.com>
2019-02-26 08:06:33 -0700

    minor change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='stephen_piccolo@byu.edu',
     packages=find_packages(exclude=['Tests*', 'docs*']),
     install_requires=['pandas', 'pyarrow', 'sqlalchemy'],
-    version=open('VERSION').read(),
+    version=open('VERSION').read().strip(),
     license='MIT',
     description='A tool for managing large datasets',
     # long_description=open('README.md').read(),
